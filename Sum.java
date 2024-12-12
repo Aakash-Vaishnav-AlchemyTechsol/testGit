495216f34552614db0d4c3683eb9da39df96a2f0
Aakash-Vaishnav-AlchemyTechsol <aakash.vp@alchemytechsol.com>
2024-12-12 10:23:56 +0530

    updated sum to total_sum

--- Sum.java
+++ Sum.java
@@ -1,9 +1,9 @@
 class Sum{
     public static void main (String[]args){
-        int sum=0;
+        int total_sum=0;
         for(int i=0;i<10;i++){
-            sum =+i;
+            total_sum =+i;
         }
-        System.out.println(sum);
+        System.out.println(total_sum);
     }
 }
