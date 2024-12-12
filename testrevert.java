edd595b46cc73e2e783598aaed7f339190e69781
Aakash-Vaishnav-AlchemyTechsol <aakash.vp@alchemytechsol.com>
2024-12-12 16:40:03 +0530

    Updated for reverting

--- testrevert.java
+++ testrevert.java
@@ -1,5 +1,5 @@
 public class testrevert{
   public static void main(String [] args){
-  System.out.println("Updated in branch1 from Local");
+  System.out.println("Updated in branch1 revert");
   }
 }
