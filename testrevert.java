9dcef87e2c55af141c5dd9d955e09e82ab7b8167
Aakash-Vaishnav-AlchemyTechsol <aakash.vp@alchemytechsol.com>
2024-12-12 16:43:31 +0530

    updating to revert2

--- testrevert.java
+++ testrevert.java
@@ -1,5 +1,9 @@
 public class testrevert{
   public static void main(String [] args){
+<<<<<<< HEAD
   System.out.println("Updated in branch1 revert");
+=======
+  System.out.println("Updated in branch1");
+>>>>>>> parent of 6158b98 (Updated from local)
   }
 }
